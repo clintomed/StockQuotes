68e3e18510494648e6a8072b269d20a7f3d46a0f
clintomed <clintonmedbery@Clintons-Mac-Pro-2.local>
2015-02-25 12:29:47 -0500

    Layout up

---
+++ app/src/main/java/com/clintonmedbery/stockquotes/LoadStock.java
@@ -0,0 +1,7 @@
+package com.clintonmedbery.stockquotes;
+
+/**
+ * Created by clintonmedbery on 2/25/15.
+ */
+public class LoadStock {
+}
--- app/src/main/java/com/clintonmedbery/stockquotes/MainActivity.java
+++ app/src/main/java/com/clintonmedbery/stockquotes/MainActivity.java
@@ -1,17 +1,43 @@
 package com.clintonmedbery.stockquotes;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.InputFilter;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    //For log printing
+    private static final String TAG ="Stock Quotes Log";
+
+    public EditText stockText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "STARTING APP");
+
         setContentView(R.layout.activity_main);
+        stockText = (EditText) findViewById(R.id.stockText);
+        stockText.setFilters(new InputFilter[] {new InputFilter.LengthFilter(4)});
+        stockText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    Log.d(TAG, "TEXT: " + stockText.getText());
+                    Stock stock = new Stock(stockText.getText().toString());
+
+                }
+                return false;
+            }
+        });
     }
 
 
