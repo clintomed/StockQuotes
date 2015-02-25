beeb5034ac610da70126896e096e28ae62e6ad84
clintomed <clintonmedbery@Clintons-Mac-Pro-2.local>
2015-02-25 13:58:43 -0500

    Toast alert working

--- app/src/main/java/com/clintonmedbery/stockquotes/MainActivity.java
+++ app/src/main/java/com/clintonmedbery/stockquotes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.clintonmedbery.stockquotes;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -119,7 +121,12 @@ public class MainActivity extends ActionBarActivity {
             super.onPostExecute(result);
             String checkValidity = stock.getLastTradeTime();
             if (checkValidity.equals("N/A")){
-                
+                Context context = getApplicationContext();
+                CharSequence text = "Symbol not found!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             } else {
                 symbolText.setText(stock.getSymbol());
                 nameText.setText(stock.getName());
